a87fdaf4b52aaf83055c3486df5e54d2587278ac
ayoubSoussi <ayoubfme@gmail.com>
2020-11-20 19:12:24 +0100

    [task01] Makefile added and module tested

--- task01/hello_module.c
+++ task01/hello_module.c
@@ -6,17 +6,17 @@
 
 static int __init hello_init(void)
 {
-	pr_alert("Hello Ayoub, this is your first module. Enjoy !");
+	pr_alert("Hello Ayoub, this is your first module. Enjoy ! \n");
 	return 0;
 }
 
-static int __exit hello_exit(void)
+static void __exit hello_exit(void)
 {
-	pr_alert("Oh ! That's sad, but I think it's time to say Godbye !");
+	pr_alert("Oh ! That's sad, but I think it's time to say Goodbye ! \n");
 }
 
 module_init(hello_init);
 module_exit(hello_exit);
-MODULE_LICENCE("GPL");
+MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("Hello module");
 MODULE_AUTHOR("Ayoub SOUSSI");
