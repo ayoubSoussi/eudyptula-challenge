49f954d1200126b03291df5a744b0e1c49a24eb4
ayoubSoussi <ayoubfme@gmail.com>
2020-11-23 16:38:28 +0100

    [task04] correct coding style of provided code and hello module

---
+++ task04/hello_module.c
@@ -0,0 +1,25 @@
+/*
+ * Licence: GPL-2.0
+ *
+ *hello_module.c
+ */
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+
+static int __init hello_init(void)
+{
+	pr_alert("Hello Ayoub, this is your first module. Enjoy !\n");
+	return 0;
+}
+
+static void __exit hello_exit(void)
+{
+	pr_alert("Oh ! That's sad, but I think it's time to say Goodbye !\n");
+}
+
+module_init(hello_init);
+module_exit(hello_exit);
+MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("Hello module");
+MODULE_AUTHOR("Ayoub SOUSSI");
